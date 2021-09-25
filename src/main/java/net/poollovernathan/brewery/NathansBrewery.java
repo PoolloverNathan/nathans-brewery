1020e0db7e769b0a0e191d48361450060265131a
PoolloverNathan <nathan0kulzer@gmail.com>
2021-09-25 08:13:13 -0400

    Merge changes

--- src/main/java/net/poollovernathan/brewery/NathansBrewery.java
+++ src/main/java/net/poollovernathan/brewery/NathansBrewery.java
@@ -20,10 +20,10 @@ public class NathansBrewery implements ModInitializer {
 	public static final Item  STEEL_NUGGET      = new Item     (new FabricItemSettings().group(ItemGroup.MISC));
 	public static final Block STEEL_BLOCK       = new Block    (FabricBlockSettings.of(Material.METAL).hardness(10).breakByTool(FabricToolTags.PICKAXES, 2));
 	public static final Item  STEEL_BLOCK_i     = new BlockItem(STEEL_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
-	public static final Block MIXER             = new Block    (FabricBlockSettings.of(Material.METAL).hardness(10).breakByTool(FabricToolTags.PICKAXES, 2));
+	public static final Block MIXER             = new Block    (FabricBlockSettings.of(Material.METAL).hardness(8));
 	public static final Item  MIXER_i           = new BlockItem(MIXER, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
-	public static final Block MIXER_FRAME       = new Block    (FabricBlockSettings.of(Material.METAL).hardness(10).breakByTool(FabricToolTags.PICKAXES, 2));
-	public static final Item  MIXER_FRAME_i     = new BlockItem(MIXER, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
+	public static final Block MIXER_FRAME       = new Block    (FabricBlockSettings.of(Material.METAL).hardness(4));
+	public static final Item  MIXER_FRAME_i     = new BlockItem(MIXER_FRAME, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
 	public static final Item  CRUSHED_COAL      = new Item     (new FabricItemSettings().group(ItemGroup.MISC));
 	public static final Item  TINY_CRUSHED_COAL = new Item     (new FabricItemSettings().group(ItemGroup.MISC));
 	public static final Item  QUILL             = new Item     (new FabricItemSettings().group(ItemGroup.TOOLS));
