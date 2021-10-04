54454e163c5fafaa3a054bf143d8e60b3f4ac594
PoolloverNathan <nathan0kulzer@gmail.com>
2021-10-04 16:01:15 -0400

    Add IEnergyItem

---
+++ src/main/java/net/poollovernathan/brewery/IEnergyItem.java
@@ -0,0 +1,10 @@
+package net.poollovernathan.brewery;
+
+public interface IEnergyItem {
+     int insertEnergy(int amount, boolean dry);
+     int extractEnergy(int amount, boolean dry);
+     boolean insertEnergyBlock(int amount, boolean dry);
+     boolean extractEnergyBlock(int amount, boolean dry);
+     int transferEnergy(IEnergyItem destination, boolean dry);
+     boolean transferEnergyBlock(IEnergyItem destination, boolean dry);
+}
